eb0be06b30bc0fa115846e71a1084e0c820b8af4
Derek Lu <scaret.in@gmail.com>
2017-09-19 17:21:49 +0800

    Removed dependency to highcharts

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import HighchartsOnly from 'highcharts';
 import ctors from './constructors.js';
 import create from './create.js';
 
 function install(Vue, options) {
-  var Highcharts = (options && options.Highcharts) || HighchartsOnly;
+  var Highcharts = options.Highcharts;
   Vue.prototype.Highcharts = Highcharts;
   for (var tagName in ctors) {
     var component = create(tagName, Highcharts, Vue);
